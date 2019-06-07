c0a404af7df4b66f77fa69fea4ff38314413ee9f
BASTIEN Valentin <valentin.bastien@sefam-medical.com>
2019-06-07 10:19:28 +0200

    Do not allow headless task execution when android app is in foreground
    Documentation :
    https://facebook.github.io/react-native/docs/headless-js-android
    
    [fix/crash-in-foreground]

--- android/src/main/java/com/jamesisaac/rnbackgroundtask/BackgroundTaskModule.java
+++ android/src/main/java/com/jamesisaac/rnbackgroundtask/BackgroundTaskModule.java
@@ -1,5 +1,7 @@
 package com.jamesisaac.rnbackgroundtask;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.util.Log;
 import com.evernote.android.job.JobManager;
 import com.evernote.android.job.JobRequest;
@@ -11,6 +13,8 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.Set;
 
@@ -146,5 +150,27 @@ public class BackgroundTaskModule extends ReactContextBaseJavaModule
         if (mJobRequest != null) {
             mJobRequest.schedule();
         }
+    }
+
+    public static boolean isAppOnForeground(Context context) {
+        /**
+         We need to check if app is in foreground otherwise the app will crash.
+         http://stackoverflow.com/questions/8489993/check-android-application-is-in-foreground-or-not
+         **/
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses =
+                activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance ==
+                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+                    appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
